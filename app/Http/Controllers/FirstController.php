fd97db8065e6dc931ea0814a94f59d2913022371
B. Israël MIENANTIMA LANDOU <berchebaisrael@gmail.com>
2026-06-25 21:37:46 +0100

    Fix:Operators names changed to lower case.

--- app/Http/Controllers/FirstController.php
+++ app/Http/Controllers/FirstController.php
@@ -639,7 +639,7 @@ class FirstController extends Controller
         $depositId = (string) Str::uuid(); 
         
         // Nom exact de l'opérateur
-        $nomOperateur = $methode === 'momo' ? 'Mobile Money' : 'Airtel Money';
+        $nomOperateur = $methode === 'momo' ? 'mobile money' : 'airtel money';
 
         // Création de l'empreinte en base (avec l'heure exacte générée par now())
         $paiement = Paiement::create([
